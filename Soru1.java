73c63b217f9592283180b5dcdf82bd60c007faea
Ala-buv <95526051+Ala-buv@users.noreply.github.com>
2022-01-31 19:08:45 +0300

    Add files via upload

---
+++ Soru1.java
@@ -0,0 +1,37 @@
+package soru1;
+
+
+public class Soru1 {
+
+
+    static void Yaz(boolean kontrol)
+    {
+        if(kontrol)
+        {
+            for(int i=1; i<6; i++)
+            {    
+                String a=String.valueOf(i);
+                String b=String.valueOf(i+1);
+                System.out.print(a+b+" ");
+            }
+        }
+        else
+        {
+    
+        for(int i=97; i<102; i++)
+            {
+                char a=(char)(i);
+                char b=(char)(i+1);
+                System.out.print(a);
+                System.out.print(b+" ");
+            }
+        }
+    }   
+
+
+    public static void main(String[] args) {
+        Yaz(true);
+        System.out.println("");
+        Yaz(false);
+    }
+}
